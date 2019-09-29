06ca2930aa959a06dc087e2b6f5992bcb417a0ab
Kezia Borges <kezia2011@hotmail.com>
2019-09-29 02:08:32 -0300

    add name and description

--- src/service.js
+++ src/service.js
@@ -5,11 +5,12 @@ function getCharacter() {
 
 async function getApiData(name) {
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/characters/${name}`);
-        document.getElementById('cname').innerHTML = (response.data);
-        console.log(response)
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?name=${name}&ts=1&apikey=354550dfaf54c663d28de06e80a30016&hash=8f5a00af4ab72412c555949e2bc247fd`);
+        document.getElementById('cname').innerHTML = response.data.data.results[0].name
+        document.getElementById('desc').innerHTML = response.data.data.results[0].description
+        console.log(response);
     } catch (error) {
-        document.getElementById('cname').innerHTML = error
+        document.getElementById('cname').innerHTML = error;
         console.log(error)
     }
 
