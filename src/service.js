1a426b29749b8756508e3c0edb8910b3a0666ddc
Kezia Borges <kezia2011@hotmail.com>
2019-10-01 15:43:30 -0300

    update js

--- src/service.js
+++ src/service.js
@@ -8,7 +8,7 @@ async function getApiData(name) {
         const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?name=${name}&ts=1&apikey=354550dfaf54c663d28de06e80a30016&hash=8f5a00af4ab72412c555949e2bc247fd`);
         document.getElementById('cname').innerHTML = response.data.data.results[0].name
         document.getElementById('desc').innerHTML = response.data.data.results[0].description
-        document.getElementById('heroesIcon').src = (response.data.data.results[0].thumbnail.path + "/portrait_xlarge" + ".jpg")
+        document.getElementById('heroesIcon').src = `${response.data.data.results[0].thumbnail.path}/portrait_xlarge.${response.data.data.results[0].thumbnail.extension}`
         console.log(response);
     } catch (error) {
         document.getElementById('cname').innerHTML = error;
